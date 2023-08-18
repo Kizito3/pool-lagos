08398ba1d55c83ccdbf7966da2ab7ef38d76436d
kizito3 <chigokizzy@gmail.com>
2023-08-18 16:56:36 +0100

    swiper fixed

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const closeBtn = document.querySelector(".close");
-const navbar = document.querySelector(".my-navbar-nav");
+const navbar = document.querySelector(".menu");
 hamburger.addEventListener("click", function () {
-  navbar.classList.add("my-active");
+  navbar.classList.add("active");
   // window.onscroll = function () {
   //   window.scrollTo(0, 0);
   // };
 });
 closeBtn.addEventListener("click", function () {
-  navbar.classList.remove("my-active");
+  navbar.classList.remove("active");
 });
-
-
-
-
-
-
-

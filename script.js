4e33ba7aa78efc4c0d0740918f1632a21b5bb67b
kizito3 <chigokizzy@gmail.com>
2023-08-19 15:57:01 +0100

    damn

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ const closeBtn = document.querySelector(".close");
 const navbar = document.querySelector(".menu");
 hamburger.addEventListener("click", function () {
   navbar.classList.add("active");
-  // window.onscroll = function () {
-  //   window.scrollTo(0, 0);
-  // };
 });
 closeBtn.addEventListener("click", function () {
   navbar.classList.remove("active");
